c37c195eb6ad9887df0f5b90bc43dc61c180db06
Arnaud Levy <contact@arnaudlevy.com>
2026-08-25 15:08:11 +0200

    circle

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params.expect(:id))
-    @people = @user.people.ordered.page(params[:page])
+    @people = @user.people_in_circle.ordered.page(params[:page])
   end
   
   def sync
